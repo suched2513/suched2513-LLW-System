3ac2832bb082a884283982491383148a7d029507
suched2513 <suched2513.pribung@gmail.com>
2026-04-20 18:44:53 +0700

    cleanup: remove diagnostic scripts

--- schema_check.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/config/database.php';
-header('Content-Type: text/plain; charset=utf-8');
-
-try {
-    $pdo = getPdo();
-    $s = $pdo->query("SHOW CREATE TABLE beh_students");
-    print_r($s->fetch());
-} catch (Exception $e) { echo $e->getMessage(); }
