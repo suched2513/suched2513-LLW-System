0fb52f7d3930fa3ded4ff378c071db0769986087
suched2513 <suched2513.pribung@gmail.com>
2026-04-20 14:38:12 +0700

    cleanup: remove debug script

--- api/list_sys_encodings.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'mb_encodings' => mb_list_encodings(),
-    'iconv_info' => function_exists('iconv') ? 'Available' : 'Missing'
-]);
