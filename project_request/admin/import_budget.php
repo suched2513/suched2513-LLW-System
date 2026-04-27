e3a22b49b2d665f519b05c8f7c9f5be6f6f930e8
suched2513 <suched2513.pribung@gmail.com>
2026-04-27 15:26:34 +0700

    debug: move error reporting to top of import_budget

--- project_request/admin/import_budget.php
+++ project_request/admin/import_budget.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+session_start();
 $pageTitle = 'นำเข้างบประมาณ';
 $pageSubtitle = 'อัปโหลดไฟล์ Excel/CSV เพื่อนำข้อมูลโครงการลงฐานข้อมูล';
 require_once __DIR__ . '/../components/layout_start.php';
