330ad47b448738d81c1cbedf616193fde9633404
suched2513 <suched2513.pribung@gmail.com>
2026-04-27 23:08:09 +0700

    cleanup: remove SBMS 2569 (project_request) system as requested

--- project_request/seed.sql
+++
@@ -1,8 +0,0 @@
--- SBMS 2569 - Seed Departments
-TRUNCATE TABLE departments;
-INSERT INTO departments (name, order_no) VALUES 
-('ฝ่ายวิชาการ', 1),
-('ฝ่ายแผนงานงบประมาณ', 2),
-('ฝ่ายบุคคล', 3),
-('ฝ่ายบริหารงานทั่วไป', 4),
-('ฝ่ายกิจการนักเรียน', 5);
