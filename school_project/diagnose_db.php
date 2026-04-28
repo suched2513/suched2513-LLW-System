fb125b81a37a0d2a2f23aa30bcb02ac06433e0f8
suched2513 <suched2513.pribung@gmail.com>
2026-04-28 23:02:03 +0700

    debug: update diagnose_db.php for more info

--- school_project/diagnose_db.php
+++ school_project/diagnose_db.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'config/db.php';
 $db = getDB();
-echo "<pre>";
-$q = $db->query("DESC llw_users");
+echo "<table border='1'><tr><th>Field</th><th>Type</th><th>Null</th><th>Default</th></tr>";
+$q = $db->query("SHOW COLUMNS FROM llw_users");
 while($r = $q->fetch()) {
-    echo $r['Field'] . " - " . $r['Type'] . "\n";
+    echo "<tr><td>{$r['Field']}</td><td>{$r['Type']}</td><td>{$r['Null']}</td><td>{$r['Default']}</td></tr>";
 }
-echo "</pre>";
+echo "</table>";
