4adcb8076fbf660cc9780253c3323b8ddb0ff1cc
suched2513 <suched2513.pribung@gmail.com>
2026-04-28 23:03:51 +0700

    security: remove diagnostic script

--- school_project/diagnose_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config/db.php';
-$db = getDB();
-echo "<table border='1'><tr><th>Field</th><th>Type</th><th>Null</th><th>Default</th></tr>";
-$q = $db->query("SHOW COLUMNS FROM llw_users");
-while($r = $q->fetch()) {
-    echo "<tr><td>{$r['Field']}</td><td>{$r['Type']}</td><td>{$r['Null']}</td><td>{$r['Default']}</td></tr>";
-}
-echo "</table>";
